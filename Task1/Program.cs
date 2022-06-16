24062c18c6bf8cf2964e5f6bcd817cc945373e39
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-06-16 18:14:17 +0300

    add Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int prompt(string message)
+{
+    Console.Write(message);  
+    string NumberStr = Console.ReadLine();
+    int number = int.Parse(NumberStr);
+    return number;
+}
+int number = prompt("Введите число: ");
+int OldNumber = number;
+int newNumber = 0;
+while (number>0)
+{
+    int A = number%10;
+    newNumber = newNumber*10 + A;
+    number = number/10;
+}
+if (newNumber == OldNumber) System.Console.WriteLine($"{OldNumber} -> да");
+else System.Console.WriteLine($"{OldNumber} -> нет");
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double prompt(string message)
+{
+    Console.Write(message);  
+    string NumberStr = Console.ReadLine();
+    double number = double.Parse(NumberStr);
+    return number;
+}
+double x1 = prompt("Введите координаты x1: ");
+double y1 = prompt("Введите координаты y1: ");
+double z1 = prompt("Введите координаты z1: ");
+double x2 = prompt("Введите координаты x2: ");
+double y2 = prompt("Введите координаты y2: ");
+double z2 = prompt("Введите координаты z2: ");
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"A ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {distance}");
